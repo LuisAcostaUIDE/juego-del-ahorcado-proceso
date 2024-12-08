e9c49f026a3564285e06ce9a4e70e89ab8c18c4c
LuisAcostaUIDE <lacostarce2@hotmail.com>
2024-12-08 17:30:59 -0500

    Update deber juegodelahorcado.py

--- deber juegodelahorcado.py
+++ deber juegodelahorcado.py
@@ -10,3 +10,18 @@ intentos_restantes = 6
 
 print("\n¡Comencemos el juego!")
 print(" ".join(palabra_mostrada))
+# 3.Comienza el juego
+while intentos_restantes > 0:
+    # Adivinar letra
+    letra = input("\nAdivina una letra: ").lower()
+    
+    if letra in palabra_secreta:
+        print("Correcto!")
+        # Actualizar las posiciones de las letras adivinadas
+        for i in range(len(palabra_secreta)):
+            if palabra_secreta[i] == letra:
+                palabra_mostrada[i] = letra
+    else:
+        print("Incorrecto. Pierdes un intento.")
+        intentos_restantes -= 1
+
